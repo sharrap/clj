9389b8f27a3a0de7a81ffc1c1cf98085417ca9be
Sean Harrap <Sean.Harrap@gmail.com>
2015-08-27 22:15:07 -0400

    Added uniq-cls method to allow uniquifying of arbitrarily complicated lists

--- src/Utils/Sequences.lisp
+++ src/Utils/Sequences.lisp
@@ -1,9 +1,10 @@
 (in-package :clj.utils)
 
-(defun uniq (ls)
+(defun uniq (&rest lsts)
   (let ((hash (make-hash-table))
         (outls NIL))
-    (loop for item in ls do (setf (gethash item hash) T))
+    (loop for ls in lsts do
+      (loop for item in ls do (setf (gethash item hash) T)))
     (with-hash-table-iterator (it hash)
       (loop
         (multiple-value-bind (entryp k v) (it)
@@ -11,4 +12,19 @@
           (if entryp
               (setf outls (cons k outls))
               (return)))))
+    outls))
+
+;Generalized uniq which supports comparison of arbitrary types.
+;Requires a hashing function and an equality test be provided
+(defun uniq-cls (hash-fn equal-fn &rest lsts)
+  (let ((hash (make-hash-table))
+        (outls NIL))
+    (loop for ls in lsts do
+      (loop for item in ls do
+        (let ((hashc (funcall hash-fn item)))
+          (multiple-value-bind (v existsp) (gethash hashc hash)
+            (when (not (and existsp (find item v :test equal-fn)))
+              (progn
+                (setf (gethash hashc hash) (cons item v))
+                (setf outls (cons item outls))))))))
     outls))
--- src/Utils/package.lisp
+++ src/Utils/package.lisp
@@ -3,7 +3,7 @@
   (:export #:make-hash-set #:set-contains #:set-add #:set-remove
            #:curry #:take #:istream #:istream-read #:istream-next
            #:hash-add-list #:split-when-1 #:split-when #:findchr
-           #:compose2 #:compose #:copylst #:uniq))
+           #:compose2 #:compose #:copylst #:uniq #:uniq-cls))
 
 (load "src/Utils/Sequences.lisp")
 (load "src/Utils/Utils.lisp")
